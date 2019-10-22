75ee94fffe50343744c6d17651eb5bcceccc36cf
paupalou <paupalou@gmail.com>
2019-10-22 18:00:09 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  padding: 1em;
+  background-color: rgb(205, 202, 169);
+
+  span:first-child {
+    font-weight: bold;
+    margin-right: 1em;
+  }
+`;
+
+const api = {
+  JSON: {'Content-Type': 'application/json'},
+  getUser: async name => {
+    const response = await fetch(`/users?name=${name}`, {
+      headers: {...api.JSON},
+    });
+    const data = await response.json();
+    if (data.length > 0) {
+      const [userData] = data;
+      return userData;
+    }
+
+    return false;
+  },
+};
+
+const sleep = (ms = Math.random() * 3000) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+const LoginForm = ({ setUser }) => {
+  const [value, setValue] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const submit = async event => {
+    event.preventDefault();
+    setLoading(true);
+    const userData = await api.getUser(value);
+    await sleep();
+    if (userData) {
+      setUser(userData);
+    }
+    setLoading(false);
+  };
+
+  if (loading) {
+    return 'loading...';
+  }
+
+  return (
+    <form onSubmit={submit}>
+      <input value={value} onChange={e => setValue(e.target.value)} />
+      <button type="submit">LogIn</button>
+    </form>
+  );
+};
+
+const User = ({ user, setUser }) => {
+  if (user) {
+    return <span>{user.name}</span>
+  }
+
+  return <LoginForm setUser={setUser} />
+}
+
+const ReservationHeader = ({user, setUser, language}) => {
+  return (
+    <Header>
+      <User user={user} setUser={setUser} />
+      <span>{language}</span>
+    </Header>
+  );
+};
+
+const App = () => {
+  const [user, setUser] = useState();
+  const [lang, setLang] = useState('es');
+
+  useEffect(() => {
+    if (user) {
+      setLang(user.language);
+    }
+  },[user, setLang]);
+
+  return (
+    <main>
+      <ReservationHeader user={user} language={lang} setUser={setUser} />
+      woot
+    </main>
+  );
+};
 
 export default App;
